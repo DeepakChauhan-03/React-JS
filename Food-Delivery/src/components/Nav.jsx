1356fac1f03369214d813f100a5fb15afa5aae8a
Deepak Chauhan <deepakchauhan6141@gmail.com>
2026-05-07 16:45:10 +0530

    Home ready

--- Food-Delivery/src/components/Nav.jsx
+++ Food-Delivery/src/components/Nav.jsx
@@ -5,20 +5,21 @@ import { LuShoppingBag } from "react-icons/lu";
 
 const Nav = () => {
   return (
-    <div className="nav w-full h-25 bg-gray-300 flex items-center justify-between p-6 ">
-        <div className='text-4xl h-[60px] w-[60px] bg-white flex items-center justify-center rounded'>
+    <div className="nav w-full h-25 flex items-center justify-between p-6 ">
+        <div className='text-4xl shadow-2xl h-[60px] w-[60px] bg-white flex items-center justify-center rounded'>
            <MdFastfood className='h-[30px] w-[30px] text-green-500' />
         </div>
        
-           <form className='flex item-center gap-10 w-[60%] h-[30px] px-5 bg-white '>
-             <IoIosSearch />
+           <form className='flex items-center  gap-10 w-[60%] h-[55px] px-5 bg-white rounded shadow-2xl'>
+             <IoIosSearch className='text-green-500 h-7.5 w-7.5' />
             <input 
-            className='border'
-            type="text" placeholder='Serach here...'/>
+            className='border h-full w-full outline-none text-2xl border-none'
+            type="text" placeholder='Search here...'/>
            </form>
         
-        <div className='text-4xl h-[60px] w-[60px] bg-white flex items-center justify-center rounded'>
-            <LuShoppingBag className='h-[30px] w-[30px] text-green-500' />
+        <div className='relative  shadow-2xl h-[60px] w-[60px] bg-white flex items-center justify-center rounded'>
+             <span className='absolute top-0.5 right-2 text-green-500 text-xl'>0</span>
+            <LuShoppingBag className='h-[30px] w-[30px] text-4xl text-green-500' />
         </div>
     </div>
   )
--- Food-Delivery/src/pages/Home.jsx
+++ Food-Delivery/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Categories from '../Categories'
 
 const Home = () => {
   return (
-    <div>
+    <div className='bg-slate-200'>
       <Nav />
+      <div className='flex flex-wrap gap-5 items-center justify-center p-8 w-full '>
+        {
+          Categories.map((item)=>{
+              return <div className='h-35 w-35 bg-white flex flex-col items-center
+               justify-center gap-4 text-2xl font-semibold cursor-pointer
+                text-gray-600 shadow-2xl rounded-lg hover:bg-green-200 transition-all duration-200'>
+                {item.icon}
+                {item.name}
+              </div>
+          })
+        }
+      </div>
     </div>
   )
 }
