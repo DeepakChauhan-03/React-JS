f028d0c6a34060c3ec6ec324690684eeaca68a2a
Deepak Chauhan <deepakchauhan6141@gmail.com>
2026-05-10 00:38:49 +0530

    card

--- Food-Delivery/src/components/Card.jsx
+++ Food-Delivery/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className='w-72 h-100 bg-white p-4 rounded-lg flex flex-col gap-3'>
+    <div className='w-72 h-100 bg-white p-4 rounded-lg flex flex-col gap-3 shadow-lg'>
          <div className='w-full h-[50%] overflow-hidden rounded-lg '>
               <img src="" alt="" className='object-cover'/>
          </div>
@@ -11,8 +11,10 @@ const Card = () => {
          </div>
          <div className='w-full flex justify-between items-center'>
               <div className='text-lg font-bold text-green-500'></div>
-              <div></div>
+              <div className='text-green-500 text-lg font-semibold'>  <span>Veg</span></div>
          </div>
+         <button className='w-full p-3 rounded-lg hover:bg-green-200 transition-all 
+          bg-green-200 text-gray-700'>Add to dish</button>
     </div>
   )
 }
